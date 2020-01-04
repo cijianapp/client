3d2c311489c7d50e993827f4b4005b4671c65940
wenyu <wenyuyang@gwu.edu>
2020-01-04 10:40:49 -0500

    commit

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import axios from "axios";
 import { connect } from "react-redux";
